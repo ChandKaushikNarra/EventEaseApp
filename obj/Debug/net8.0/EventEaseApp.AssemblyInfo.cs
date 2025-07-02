4f1fafe619e1bb465e491a396a2c482b3f81f19c
Chand Kaushik Narra <chandkaushik.narra@gmail.com>
2025-07-02 14:38:21 +0530

    Remove @page from EventCard and update usage

--- obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8d9ab616387604b6964160a53456aba005d14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfc035d524a4d62aece2801cfbc0c3d404d9880")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
